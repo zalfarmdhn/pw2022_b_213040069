5c3a78149e8b73aa422dea3ba89f28727a09ffeb
zalfarmdhn <zalfarmdhn1011@gmail.com>
2022-03-04 13:27:38 +0700

    menambah file pertemuan3

---
+++ Kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,25 @@
+<?php
+// Pengulangan
+
+// for
+for ($i = 0; $i < 5; $i++) {
+    echo "Hello World! (pengulangan for) <br>";
+}
+
+echo "<br>";
+
+// while
+$i = 0;
+while ($i < 5) {
+    echo "Hello World! (pengulangan while) <br>";
+    $i++;
+}
+
+echo "<br>";
+
+// do.. while
+$i = 0;
+do {
+    echo "Hello World! (pengulangan do.. while) <br>";
+    $i++;
+} while ($i < 5);
---
+++ Kuliah/pertemuan3/latihan3.php
@@ -0,0 +1,16 @@
+<?php
+// Pengkondisian / Percabangan
+
+// if else
+$x = 20;
+if ($x < 20) {
+    echo "classified";
+} elseif ($x == 20) {
+    echo "jackpot";
+} else {
+    echo "covert";
+}
+
+// if else if else
+// ternary
+// switch
